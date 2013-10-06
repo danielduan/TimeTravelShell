3d95e0309b362e9f6a332a8477004da9ae6a26d0
Daniel Duan <danielduan88@yahoo.com>
2013-10-05 20:06:16 -0700

    latest

--- read-command.c
+++ read-command.c
@@ -390,6 +390,7 @@ command_t make_command(token_container* list) {
         } // check if && or || or | is present
         else if (peek(&operators) != NULL && (peek(&operators)->type == AND_COMMAND || peek(&operators)->type == OR_COMMAND || peek(&operators)->type == PIPE_COMMAND)) {
           command_t words = peek(&operators);
+          words->u.word = NULL;
           pop(&operators);
 
           words->input = NULL;
