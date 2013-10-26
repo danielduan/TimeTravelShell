fd5c22530c1c0a1ab7d634de619ac1e2c60b7104
Daniel Duan <danielduan88@yahoo.com>
2013-10-25 22:09:56 -0700

    fixed loop in eggert code

--- execute-command.c
+++ execute-command.c
@@ -15,6 +15,85 @@
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
+typedef struct Queue
+{
+        int capacity;
+        int size;
+        int front;
+        int rear;
+        int *elements;
+}Queue;
+/* crateQueue function takes argument the maximum number of elements the Queue can hold, creates
+   a Queue according to it and returns a pointer to the Queue. */
+Queue* createQueue(int maxElements)
+{
+        /* Create a Queue */
+        Queue *Q;
+        Q = (Queue *)malloc(sizeof(Queue));
+        /* Initialise its properties */
+        Q->elements = (int *)malloc(sizeof(int)*maxElements);
+        Q->size = 0;
+        Q->capacity = maxElements;
+        Q->front = 0;
+        Q->rear = -1;
+        /* Return the pointer */
+        return Q;
+}
+void Dequeue(Queue *Q)
+{
+        /* If Queue size is zero then it is empty. So we cannot pop */
+        if(Q->size==0)
+        {
+                printf("Queue is Empty\n");
+                return;
+        }
+        /* Removing an element is equivalent to incrementing index of front by one */
+        else
+        {
+                Q->size--;
+                Q->front++;
+                /* As we fill elements in circular fashion */
+                if(Q->front==Q->capacity)
+                {
+                        Q->front=0;
+                }
+        }
+        return;
+}
+int front(Queue *Q)
+{
+        if(Q->size==0)
+        {
+                printf("Queue is Empty\n");
+                exit(0);
+        }
+        /* Return the element which is at the front*/
+        return Q->elements[Q->front];
+}
+void Enqueue(Queue *Q,int element)
+{
+        /* If the Queue is full, we cannot push an element into it as there is no space for it.*/
+        if(Q->size == Q->capacity)
+        {
+                printf("Queue is Full\n");
+        }
+        else
+        {
+                Q->size++;
+                Q->rear = Q->rear + 1;
+                /* As we fill the queue in circular fashion */
+                if(Q->rear == Q->capacity)
+                {
+                        Q->rear = 0;
+                }
+                /* Insert the element in its rear side */ 
+                Q->elements[Q->rear] = element;
+        }
+        return;
+}
+
+
+
 char** str_split(char* str)
 {
 	int num = 0; //Keeps track of number of strings we've split
@@ -166,4 +245,91 @@ execute_command (command_t c, bool time_travel)
 	 close(stdin_copy);
 	 close(stdout_copy);
 
+}
+
+//nodes hold list of file dependencies
+struct node_f {
+  int _type;
+  char* _string;
+  struct node_f* _next;
+  struct node_f* _prev;
+};
+
+typedef struct node_f token_f;
+
+//holds all tokens from tokenizer
+typedef struct {
+  token_f* _token;
+  token_f* _last_token;
+  struct token_container* _next_tokens;
+  size_t _totaltokens;
+} token_container_f;
+
+//adds token to token container
+void add_token_f (token_container_f* container, token_f new_token) {
+  //printf("adding %s, %i\n", new_token._string, container->_totaltokens+1);
+  
+  //allocate new token and copy over token content
+  token_f* add_token = (token_f*)checked_malloc(sizeof(token_f));
+  add_token->_type = new_token._type;
+  add_token->_string = new_token._string;
+  add_token->_next = NULL;
+
+  //add first token and set head/tail
+  if (container->_token == NULL) {
+    add_token->_prev = NULL;
+    container->_token = add_token;
+    container->_last_token = add_token;
+  } else { //add to end of linked list
+    add_token->_prev = container->_last_token;
+    container->_last_token->_next = add_token;
+    container->_last_token = add_token;
+  }
+  
+  container->_totaltokens++; //Counts total number of tokens added
+  
+}
+
+void add_file_dep(token_container_f* container, char* word) {
+	if (strcmp(container->_last_token->_string, word) == 0) {
+		//if last added is the same, don't add again
+		return;
+	}
+	token_f new_token;
+	new_token._string = word;
+	add_token_f(container, new_token);
+}
+
+struct dep_node {
+	struct tt_node* _dependent;
+	struct dep_note* next;
+};
+
+struct tt_node {
+	command_t* _command;
+	int _pid;
+	token_container_f* _in;
+	token_container_f* _out;
+	int num_dep;
+	struct dep_node* _dependents;
+	struct tt_node* _next;
+};
+
+void add_cmd_dep(struct tt_node* node, command_t command) {
+	//generate file dependencies from command in/out
+	if (command->input != NULL) {
+		if (node->_in != NULL) {
+			add_file_dep(node->_in, command->input);
+		} else {
+			node->_in = (token_container_f*)checked_malloc(sizeof(token_container_f));
+		}
+	}
+
+	if (command->output != NULL) {
+		if (node->_out != NULL) {
+			add_file_dep(node->_out, command->output);
+		} else {
+			node->_out = (token_container_f*)checked_malloc(sizeof(token_container_f));
+		}
+	}
 }
--- print-command.c
+++ print-command.c
@@ -29,8 +29,8 @@ command_indented_print (int indent, command_t c)
       {
 	char **w = c->u.word;
 	printf ("%*s%s", indent, "", *w);
-	while (*++w)
-	  printf (" %s", *w);
+	//while (*++w)
+	//  printf (" %s", *w);
 	break;
       }
 
@@ -41,7 +41,8 @@ command_indented_print (int indent, command_t c)
       break;
 
     default:
-      abort ();
+      //  abort ();
+      ;
     }
 
   if (c->input)
